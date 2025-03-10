240d74ba51adb594d130a43066820ccea47285ef
Shashikanth Reddy <kolanshashii@gmail.com>
2025-03-10 13:28:11 -0400

    feat: Update transition time

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,12 +4,9 @@ interface ProgressBarProps {
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
   return (
-    <div className="progress-bar" style={{ opacity: progress > 0 ? 1 : 0 }}>
+    <div className="progress-bar">
       {/* Width of the progress bar is dynamically set based on the 'progress' prop */}
-      <div
-        className="progress"
-        style={{ width: `${progress}%`, opacity: progress > 0 ? 1 : 0 }}
-      />
+      <div className="progress" style={{ width: `${progress}%` }} />
     </div>
   );
 };
